350287b65e45edcc212f59e1a417263d8bd7f876
Julius Masigan <juliusmasigan@gmail.com>
2015-08-23 23:48:25 +0800

    Transform Request.

--- js/services/verificationService.js
+++ js/services/verificationService.js
@@ -1,7 +1,10 @@
 var verificationService = angular.module('verificationService', ['ngResource']);
 
 verificationService.factory('Verify', ['$resource', function($resource) {
-    return $resource('http://192.168.0.43:8000/verify/', {}, {
-        post: {method:'POST', withCredentials:true}
+    return $resource('http://localhost:8000/verify/', {}, {
+        post: {
+            method:'POST', 
+            withCredentials:true,
+        }
     }, {stripTrailingSlashes:false});
 }]);
