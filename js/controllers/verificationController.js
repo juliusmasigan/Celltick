09e624ce3fdad7e5002519c00be418f7c1a58cb4
Julius Masigan <jmasigan@xyber.ph>
2015-08-24 19:04:39 +0800

    Added validation. Checks form before submitting the verification form.

--- js/controllers/verificationController.js
+++ js/controllers/verificationController.js
@@ -2,9 +2,57 @@ var verificationController = angular.module('verificationController', ['verifica
 
 verificationController.controller('verify', ['$scope', 'Verify', function($scope, Verify) {
     $('#mobile-modal').openModal();
-
     $scope.msisdn;
+    $scope.verif_code;
+
     $scope.submit_mobile = function(clickEvent) {
-        var verif = Verify.post({},{'msisdn':$scope.msisdn});
+        if($scope.msisdn == undefined || $scope.msisdn.length == 0) {
+            $('label[for=msisdn]').attr({'data-error':'Please enter your mobile number.'}).addClass('invalid');
+            $('input#msisdn').addClass('invalid').focus();
+            return;
+        }
+
+        Verify.post({},{'msisdn':$scope.msisdn}).$promise.then(
+            function(result) {
+                $('#mobile-modal').closeModal();
+                $('#verif-modal').openModal();
+            }, function(error) {
+                //Handle error here.
+                if(["AUTH41"].indexOf(error.data.code) != -1 && error.status == 422) {
+                    $('#mobile-modal').closeModal();
+                    $('#verif-modal').openModal();
+                }
+            }
+        );
     };
+
+    $scope.submit_verification = function(clickEvent) {
+        if($scope.verif_code == undefined || $scope.verif_code.length == 0) {
+            $('label[for=verification_code]').attr({'data-error':'Please enter verification code.'}).addClass('invalid');
+            $('input#verification_code').addClass('invalid').focus();
+            return;
+        }
+
+        Verify.post({}, {'verification_code':$scope.verif_code}).$promise.then(
+            function(result) {
+                $('#verif-modal').closeModal();
+            }, function(error) {
+                //Handle error here.
+                var errorMsg;
+                if(error.status == 400 && ["AUTH42"].indexOf(error.data.code) != -1){
+                    errorMsg = "Invalid verification code.";
+                }else if(error.status == 422) {
+                    errorMsg = "Please enter verification code.";
+                }
+                $('label[for=verification_code]').attr({'data-error':errorMsg}).addClass('invalid');
+                $('input#verification_code').addClass('invalid').focus();
+            }
+        );
+    };
+
+    $('input#verification_code').blur(function(event) {
+        if(this.value.length == 0) {
+            $(this).removeClass('invalid');
+        }
+    });
 }]);
--- js/services/verificationService.js
+++ js/services/verificationService.js
@@ -1,10 +1,17 @@
 var verificationService = angular.module('verificationService', ['ngResource']);
 
 verificationService.factory('Verify', ['$resource', function($resource) {
-    return $resource('http://localhost:8000/verify/', {}, {
+    return $resource('http://192.168.0.43:8000/verify/', {}, {
         post: {
             method:'POST', 
             withCredentials:true,
+            interceptor: {
+                response: function(response) {
+                    var result = response.resource;
+                    result.status = response.status;
+                    return result;
+                }
+            }
         }
     }, {stripTrailingSlashes:false});
 }]);
