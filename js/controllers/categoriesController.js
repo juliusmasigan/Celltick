9ead427e346cff60a9adb0ccad4f6207916b6cc2
Julius Masigan <jmasigan@xyber.ph>
2015-08-25 20:18:49 +0800

    Removed previous card view in home page.

--- js/controllers/categoriesController.js
+++
@@ -1,7 +0,0 @@
-var categoriesController = angular.module('categoriesController', []);
-
-categoriesController.controller('categoryCardsController', ['$scope', 'Category', function($scope, Category) {
-    var categories = Category.query();
-
-    $scope.categories = categories;
-}]);
--- js/services/categoryService.js
+++
@@ -1,7 +0,0 @@
-var categoryService = angular.module('categoryService', ['ngResource']);
-
-categoryService.factory('Category', ['$resource', function($resource) {
-    return $resource('/temp_data/categories.json', {}, {
-        query: {method:'GET', isArray:true}
-    });
-}]);
