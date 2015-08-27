daedc47d7e665182f0270a250dcd61cf6db4df6c
Julius Masigan <juliusmasigan@gmail.com>
2015-08-28 00:28:51 +0800

    Renamed file.

--- js/services/buy.js
+++
@@ -1,7 +0,0 @@
-var buyService = angular.module('buyService', []);
-
-buyService.factory('Buy', ['$resource', function($resource) {
-    return $resource(celltickApp.apiBaseUrl+"/buy/:content_id", {}, {
-        
-    }, {stripTrailingSlashes:false});
-}]);
