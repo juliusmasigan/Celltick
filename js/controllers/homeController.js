b014260775d11bf4941ff20b4f0dea18b79f2960
Julius Masigan <jmasigan@xyber.ph>
2015-09-02 17:54:46 +0800

    Removed the buy function inside the scope.

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -4,24 +4,6 @@ homeController.controller('homeCardsController', ['$scope', '$window', 'Groupset
     $scope.verification = "pages/verification-modal.html";
     $scope.apiBaseUrl = celltickApp.apiBaseUrl;
 
-    $scope.buy = function(content) {
-        cid = content.id.replace(/\-/g, "");
-        Buy.get({content_id:cid}).$promise.then(
-            function(result) {
-                url = result.grant_url.replace(/\-/g, "");
-                var form = document.createElement('form');
-                form.setAttribute('method', "GET");
-                form.setAttribute('action', celltickApp.apiBaseUrl+"/download/"+url);
-                form.submit();
-                //$window.location.href = celltickApp.apiBaseUrl+"/download/"+url;
-            }, function(error) {
-                if(error.status == 401) {
-                    $('#mobile-modal').openModal();
-                }
-            }
-        );
-    };
-
     Groupset.query({name:'home'}).$promise.then(
         function(result) {
             $scope.categories = result;
