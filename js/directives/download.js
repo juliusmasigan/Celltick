cd378530daa11900b38257eed0938be66c82a4e3
Julius Masigan <jmasigan@xyber.ph>
2015-09-02 17:56:53 +0800

    Add buy function.

--- js/directives/download.js
+++ js/directives/download.js
@@ -1,14 +1,32 @@
-var ctdownload = angular.module('ctdownload', []);
+var ctdownload = angular.module('ctdownload', ['buyService']);
 
-ctdownload.directive('ctDownload', function() {
+ctdownload.directive('ctDownload', ['$window', 'Buy', function($window, Buy) {
     return {
         restrict:'E',
-        template:"<button class='btn' ng-click=$emit('buy')>Download for PHP {{price}}</button>",
+        template:"<button class='btn center-align' ng-click=$emit('buy')>Download for PHP {{price}}</button>",
         link:function($scope, elem, attrs) {
             $scope.price = $scope.content.price;
+            $scope.buy = function(content) {
+                cid = content.id.replace(/\-/g, "");
+                Buy.get({content_id:cid}).$promise.then(
+                    function(result) {
+                        url = result.grant_url.replace(/\-/g, "");
+                        /*var form = document.createElement('form');
+                        form.setAttribute('method', "GET");
+                        form.setAttribute('action', celltickApp.apiBaseUrl+"/download/"+url);
+                        form.submit();*/
+                        $window.location.href = celltickApp.apiBaseUrl+"/download/"+url;
+                    }, function(error) {
+                        if(error.status == 401) {
+                            $('#mobile-modal').openModal();
+                        }
+                    }
+                );
+            };
+
             $scope.$on('buy', function() {
                 $scope.$eval($scope.buy($scope.content));
             });
         }
     };
-});
+}]);
